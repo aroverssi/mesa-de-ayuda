74f325cc3c210b07be8b4f087fc9ad48febfe9c3
aroverssi <aroverssi@gmail.com>
2024-11-20 17:34:12 -0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,8 +1,35 @@
 // Importar las funciones necesarias desde el SDK de Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore, collection, addDoc, doc, getDoc, updateDoc, increment, setDoc, onSnapshot, query, orderBy, where, limit, startAfter, endBefore, limitToLast, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { deleteDoc, doc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+// Importar las funciones necesarias desde el SDK de Firebase
+import { 
+    initializeApp 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    doc, 
+    getDoc, 
+    updateDoc, 
+    increment, 
+    setDoc, 
+    onSnapshot, 
+    query, 
+    orderBy, 
+    where, 
+    limit, 
+    startAfter, 
+    endBefore, 
+    limitToLast, 
+    getDocs, 
+    deleteDoc 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+import { 
+    getAuth, 
+    signInWithEmailAndPassword 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
 
 
 // Configuración de Firebase
